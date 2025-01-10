cf36e6b5df37e745a2e277beb54eef68b786bdf1
Cassian Godsted <cassian@godsted.com>
2025-01-10 12:19:22 -0500

    Port to 1.20

--- src/main/java/cc/cassian/raspberry/registry/RaspberryBlocks.java
+++ src/main/java/cc/cassian/raspberry/registry/RaspberryBlocks.java
@@ -1,21 +1,22 @@
 package cc.cassian.raspberry.registry;
 
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import oshi.util.tuples.Pair;
-import vectorwing.farmersdelight.FarmersDelight;
 import vectorwing.farmersdelight.common.block.StoveBlock;
 import vectorwing.farmersdelight.common.registry.ModBlocks;
+import vectorwing.farmersdelight.common.registry.ModCreativeTabs;
 import xanthian.copperandtuffbackport.blocks.custom.GrateBlock;
 
 import java.util.function.Supplier;
@@ -27,20 +28,20 @@ public class RaspberryBlocks {
 
     public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>>
             SILT_STOVE = registerBlock("silt_stove",
-            ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
+            ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), ModCreativeTabs.TAB_FARMERS_DELIGHT.getKey());
 
     public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>>
             ASH_STOVE = registerBlock("ash_stove",
-            ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
+            ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), ModCreativeTabs.TAB_FARMERS_DELIGHT.getKey());
 
     public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>>
             LEAD_GRATE = registerBlock("lead_grate",
-            ()-> new GrateBlock(BlockBehaviour.Properties.of(Material.HEAVY_METAL).noOcclusion().strength(5.0F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.METAL)), CreativeModeTab.TAB_BUILDING_BLOCKS);
+            ()-> new GrateBlock(BlockBehaviour.Properties.of().noOcclusion().strength(5.0F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.METAL)), CreativeModeTabs.BUILDING_BLOCKS);
 
 
-    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>> registerBlock(String blockID, Supplier<Block> blockSupplier, CreativeModeTab tab) {
+    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>> registerBlock(String blockID, Supplier<Block> blockSupplier, @Nullable ResourceKey<CreativeModeTab> tab) {
         final var block = BLOCKS.register(blockID, blockSupplier);
-        final var item = RaspberryItems.ITEMS.register(blockID, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+        final var item = RaspberryItems.ITEMS.register(blockID, () -> new BlockItem(block.get(), new Item.Properties()));
         return new Pair<>(block, item);
     }
 
