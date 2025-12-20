6e825c30f0702c514e81e68e49886eccb7a58d1a
Cassian Godsted <cassian@godsted.com>
2025-12-20 18:09:27 -0500

    add placeholder effects

--- src/main/java/cc/cassian/raspberry/registry/RaspberryMobEffects.java
+++ src/main/java/cc/cassian/raspberry/registry/RaspberryMobEffects.java
@@ -4,11 +4,13 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.*;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Map;
 
 import static cc.cassian.raspberry.RaspberryMod.MOD_ID;
 
@@ -27,6 +29,11 @@ public class RaspberryMobEffects {
 
     public static final RegistryObject<MobEffect> COUGHING = MOB_EFFECTS.register("coughing",
             () -> new Coughing(MobEffectCategory.HARMFUL, 10076657)
+    );
+
+    public static final RegistryObject<MobEffect> PANIC = MOB_EFFECTS.register("panic",
+            () -> new Panic(MobEffectCategory.BENEFICIAL, 15494786)
+            .addAttributeModifier(Attributes.MOVEMENT_SPEED, "CE4EFE3F-12D8-4C0A-AA36-312EEE9DBEF4", 0.2F, AttributeModifier.Operation.MULTIPLY_TOTAL)
     );
 
     private static class Aftershock extends MobEffect {
@@ -49,6 +56,22 @@ public class RaspberryMobEffects {
         @Override
         public void applyEffectTick(LivingEntity livingEntity, int amplifier) {
             livingEntity.hurt(livingEntity.damageSources().generic(), amplifier+1);
+        }
+
+        @Override
+        public boolean isDurationEffectTick(int duration, int amplifier) {
+            int j = 25 >> amplifier;
+            if (j > 0) {
+                return duration % j == 0;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    private static class Panic extends MobEffect {
+        public Panic(MobEffectCategory category, int color) {
+            super(category, color);
         }
     }
 }
