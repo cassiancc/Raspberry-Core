097fc0027bf178e681505f64ec37ee9357e04676
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-03 04:10:03 -0800

    Fix crash with Pehkui

--- src/main/java/cc/cassian/raspberry/mixin/minecraft/MobRendererMixin.java
+++ src/main/java/cc/cassian/raspberry/mixin/minecraft/MobRendererMixin.java
@@ -25,10 +25,8 @@ THE SOFTWARE.
 package cc.cassian.raspberry.mixin.minecraft;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -37,13 +35,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MobRenderer.class)
 public abstract class MobRendererMixin<T extends Mob> extends EntityRendererMixin<T> {
-
     @Inject(
-        method = "renderLeash",
-        at = @At("HEAD"),
-        cancellable = true
+            method = "render(Lnet/minecraft/world/entity/Mob;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/entity/MobRenderer;renderLeash(Lnet/minecraft/world/entity/Mob;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/world/entity/Entity;)V"
+            ),
+            cancellable = true
     )
-    private void raspberry$cancelVanillaLeash(T mob, float partialTick, PoseStack poseStack, MultiBufferSource buffer, Entity holder, CallbackInfo ci) {
+    private void onRenderLeash(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
         ci.cancel();
     }
 }
