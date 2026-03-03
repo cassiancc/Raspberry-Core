18d65492e04cfb6f1285f2a6cf5d7d7593767b4c
Cassian <cassian@godsted.com>
2026-03-03 12:44:59 -0500

    Fully replace CC golems

--- src/main/java/cc/cassian/raspberry/compat/CopperAgeBackportCompat.java
+++ src/main/java/cc/cassian/raspberry/compat/CopperAgeBackportCompat.java
@@ -1,6 +1,7 @@
 package cc.cassian.raspberry.compat;
 
 import cc.cassian.raspberry.mixin.copperagebackport.CopperGolemEntityAccessor;
+import com.github.smallinger.copperagebackport.ModMemoryTypes;
 import com.github.smallinger.copperagebackport.entity.CopperGolemEntity;
 import com.github.smallinger.copperagebackport.registry.ModEntities;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
@@ -15,26 +16,26 @@ import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class CopperAgeBackportCompat {
-	public static void spawnCopperGolem(Level level, BlockPos pos, LocalRef<LivingEntity> golem, BlockState state) {
-		BlockState abovestate = level.getBlockState(pos.above());
-		CopperGolemEntity coppergolem = ModEntities.COPPER_GOLEM.get().create(level);
-		if (coppergolem == null) {return;}
+	public static void spawnCopperGolem(Level level, BlockPos pos, LocalRef<LivingEntity> golem, float yRot, BlockState abovestate) {
+		CopperGolemEntity copperGolem = ModEntities.COPPER_GOLEM.get().create(level);
+		if (copperGolem == null) {return;}
 
 		if (abovestate.is(Blocks.LIGHTNING_ROD) || abovestate.is(CCBlocks.WAXED_LIGHTNING_ROD.get())) {
-			coppergolem.setWeatherState(WeatheringCopper.WeatherState.UNAFFECTED);
+			copperGolem.spawn(WeatheringCopper.WeatherState.UNAFFECTED);
 		} else if (abovestate.is(CCBlocks.EXPOSED_LIGHTNING_ROD.get()) || abovestate.is(CCBlocks.WAXED_EXPOSED_LIGHTNING_ROD.get())) {
-			coppergolem.setWeatherState(WeatheringCopper.WeatherState.EXPOSED);
+			copperGolem.spawn(WeatheringCopper.WeatherState.EXPOSED);
 		} else if (abovestate.is(CCBlocks.WEATHERED_LIGHTNING_ROD.get()) || abovestate.is(CCBlocks.WAXED_WEATHERED_LIGHTNING_ROD.get())) {
-			coppergolem.setWeatherState(WeatheringCopper.WeatherState.WEATHERED);
+			copperGolem.spawn(WeatheringCopper.WeatherState.WEATHERED);
 		} else if (abovestate.is(CCBlocks.OXIDIZED_LIGHTNING_ROD.get()) || abovestate.is(CCBlocks.WAXED_OXIDIZED_LIGHTNING_ROD.get())) {
-			coppergolem.setWeatherState(WeatheringCopper.WeatherState.OXIDIZED);
+			copperGolem.spawn(WeatheringCopper.WeatherState.OXIDIZED);
 		}
+		copperGolem.getBrain().setMemory(ModMemoryTypes.TRANSPORT_ITEMS_COOLDOWN_TICKS.get(), 140);
 
 		if (abovestate.is(CCBlockTags.WAXED_COPPER_BLOCKS)) {
-			((CopperGolemEntityAccessor) coppergolem).setNextWeatheringTick(-2L);
+			((CopperGolemEntityAccessor) copperGolem).setNextWeatheringTick(-2L);
 		}
 
-		golem.set(coppergolem);
-		coppergolem.moveTo((double)pos.getX() + (double)0.5F, (double)pos.getY() + 0.05, (double)pos.getZ() + (double)0.5F, state.getValue(CarvedPumpkinBlock.FACING).toYRot(), 0.0F);
+		golem.set(copperGolem);
+		copperGolem.moveTo((double)pos.getX() + (double)0.5F, (double)pos.getY() + 0.05, (double)pos.getZ() + (double)0.5F, yRot, 0.0F);
 	}
 }
--- src/main/java/cc/cassian/raspberry/mixin/caverns_and_chasms/CopperGolemMixin.java
+++ src/main/java/cc/cassian/raspberry/mixin/caverns_and_chasms/CopperGolemMixin.java
@@ -7,6 +7,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.teamabnormals.caverns_and_chasms.common.entity.animal.CopperGolem;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -15,13 +16,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.DataInput;
+
 @Mixin(CopperGolem.class)
 public class CopperGolemMixin {
 	@Inject(
-			method = "createGolem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;moveTo(DDDFF)V"))
-	private static void raspberry$spawnABetterGolem(Level level, BlockPos pos, BlockState state, CallbackInfo ci, @Local LocalRef<LivingEntity> golem) {
+			method = "createGolem", remap = false, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;moveTo(DDDFF)V", remap = true))
+	private static void raspberry$spawnABetterGolem(Level level, BlockPos pos, BlockState state, CallbackInfo ci, @Local LocalRef<LivingEntity> golem, @Local(name = "abovestate") BlockState aboveState, @Local(name = "yRot") float yRot) {
 		if (ModConfig.get().betterCopperGolems && ModCompat.hasCopperAgeBackport()) {
-			CopperAgeBackportCompat.spawnCopperGolem(level, pos, golem, state);
+			CopperAgeBackportCompat.spawnCopperGolem(level, pos, golem, yRot, aboveState);
 		}
 	}
 
