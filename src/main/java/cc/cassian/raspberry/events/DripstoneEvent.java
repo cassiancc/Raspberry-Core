1be701782081c8ae4e7eb8dddf3e4691e1076bd6
Cassian <cassian@godsted.com>
2026-07-22 23:39:11 -0400

    geode testing

--- src/main/java/cc/cassian/raspberry/events/DripstoneEvent.java
+++ src/main/java/cc/cassian/raspberry/events/DripstoneEvent.java
@@ -22,30 +22,37 @@ import java.util.function.Predicate;
 public class DripstoneEvent {
 
 	/**
-	 * @param level - The level this conversion is taking place in.
-	 * @param blockPos - The position of the dripstone.
-	 * @param fluidInfo - The fluid being tested for conversion.
-	 * @param fluid - The fluid being tested for conversion.
-	 * @param targetBlock - The block we want to convert to.
-	 * @param convertsToTarget - The blocks that are possible to convert from.
-	 * @param requiredFluid - The fluid required for conversion to take place.
-	 * @param requiredBlock - The block required below the fluid for conversion to take place.
+	 * @param level The level this conversion is taking place in.
+	 * @param blockPos The position of the dripstone.
+	 * @param fluidInfo The fluid being tested for conversion.
+	 * @param fluid The fluid being tested for conversion.
+	 * @param targetBlockState The block we want to convert to.
+	 * @param convertsToTarget The blocks that are possible to convert from.
+	 * @param requiredFluid The fluid required for conversion to take place.
+	 * @param requiredBlock The block required below the fluid for conversion to take place.
+	 * @param generateAsCrystal Whether to place the requiredBlock above the block being converted.
 	 * <br><br>
 	 * In the world, this would look like:
 	 * <pre>{@code
 	 * requiredFluid/fluid/fluidInfo (usually water)
 	 * requiredBlock (rock salt)
 	 * blockPos (pointed dripstone block)
+	 * (air) - (with generateAsCrystal, targetBlock is placed here)
 	 * convertsToTarget->targetBlock (dripstone, converted to rock salt)
 	 * }</pre>
 	 */
-	public static void convertBlockViaDripstone(ServerLevel level, BlockPos blockPos, PointedDripstoneBlock.FluidInfo fluidInfo, Fluid fluid, Block targetBlock, TagKey<Block> convertsToTarget, FlowingFluid requiredFluid, Block requiredBlock) {
+	public static void convertBlockViaDripstone(ServerLevel level, @Nullable BlockPos blockPos, PointedDripstoneBlock.FluidInfo fluidInfo, Fluid fluid, BlockState targetBlockState, TagKey<Block> convertsToTarget, FlowingFluid requiredFluid, Block requiredBlock, boolean generateAsCrystal) {
+		if (blockPos == null) return;
 		if (level.getBlockState(fluidInfo.pos().below()).is(requiredBlock) && fluid == requiredFluid) {
-			BlockPos blockPos2 = findConvertible(level, blockPos, convertsToTarget);
-			if (blockPos2 != null) {
+			BlockPos targetPos = findConvertible(level, blockPos, convertsToTarget);
+			if (targetPos != null) {
 				level.levelEvent(LevelEvent.DRIPSTONE_DRIP, blockPos, 0);
-				BlockState blockState = targetBlock.defaultBlockState();
-				level.setBlockAndUpdate(blockPos2, blockState);
+				BlockPos above = targetPos.above();
+				if (generateAsCrystal & level.getBlockState(above).isAir()) {
+					level.setBlockAndUpdate(above, targetBlockState);
+				} else if (!generateAsCrystal) {
+					level.setBlockAndUpdate(targetPos, targetBlockState);
+				}
 			}
 		}
 	}
--- src/main/java/cc/cassian/raspberry/mixin/spelunkery/PointedDripstoneBlockMixin.java
+++ src/main/java/cc/cassian/raspberry/mixin/spelunkery/PointedDripstoneBlockMixin.java
@@ -4,13 +4,22 @@ import cc.cassian.raspberry.ModCompat;
 import cc.cassian.raspberry.compat.SpelunkeryCompat;
 import cc.cassian.raspberry.events.DripstoneEvent;
 import cc.cassian.raspberry.registry.RaspberryTags;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.ordana.spelunkery.reg.ModBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.PointedDripstoneBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,10 +30,13 @@ import java.util.Optional;
 @Mixin(PointedDripstoneBlock.class)
 public class PointedDripstoneBlockMixin {
 
-    @Inject(method = "maybeTransferFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", shift = At.Shift.BEFORE))
+    @Inject(method = "maybeTransferFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/PointedDripstoneBlock;findTip(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;IZ)Lnet/minecraft/core/BlockPos;"))
     private static void soSalty(BlockState state, ServerLevel level, BlockPos pos, float randChance, CallbackInfo ci, @Local Optional<PointedDripstoneBlock.FluidInfo> optional, @Local Fluid fluid, @Local(ordinal = 0) BlockPos blockPos) {
         if (ModCompat.SPELUNKERY) {
-            DripstoneEvent.convertBlockViaDripstone(level, pos, optional.orElseThrow(), fluid, SpelunkeryCompat.rockSalt, RaspberryTags.CONVERTS_TO_SALT, Fluids.WATER, SpelunkeryCompat.rockSalt);
+            DripstoneEvent.convertBlockViaDripstone(level, pos, optional.orElseThrow(), fluid, SpelunkeryCompat.rockSalt.defaultBlockState(), RaspberryTags.CONVERTS_TO_SALT, Fluids.WATER, SpelunkeryCompat.rockSalt, false);
+        }
+        if (ModCompat.QUARK) {
+            DripstoneEvent.convertBlockViaDripstone(level, pos, optional.orElseThrow(), fluid, ForgeRegistries.BLOCKS.getValue(ResourceLocation.fromNamespaceAndPath("quark", "blue_corundum_cluster")).defaultBlockState().setValue(BlockStateProperties.FACING, Direction.UP), BlockTags.DIAMOND_ORES, Fluids.LAVA, ModBlocks.ROUGH_DIAMOND_BLOCK.get(), true);
         }
     }
 
