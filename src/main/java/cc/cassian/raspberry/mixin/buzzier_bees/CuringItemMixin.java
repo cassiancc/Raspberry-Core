4d10434fe592b3267bef7d36f0c21467a6102e1c
Cassian <cassian@godsted.com>
2026-08-24 12:29:43 -0400

    non-stupid honey bottle mixin
    fixes #68

--- src/main/java/cc/cassian/raspberry/mixin/buzzier_bees/CuringItemMixin.java
+++ src/main/java/cc/cassian/raspberry/mixin/buzzier_bees/CuringItemMixin.java
@@ -1,8 +1,11 @@
 package cc.cassian.raspberry.mixin.buzzier_bees;
 
 import cc.cassian.raspberry.config.ModConfig;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.teamabnormals.blueprint.common.advancement.EmptyTrigger;
 import com.teamabnormals.buzzier_bees.common.item.CuringItem;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -13,10 +16,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CuringItem.class)
 public class CuringItemMixin {
-	@Inject(
-			method = "finishUsingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;awardStat(Lnet/minecraft/stats/Stat;)V"), cancellable = true)
-	public void raspberry$disableCuring(ItemStack stack, Level worldIn, LivingEntity entityLiving, CallbackInfoReturnable<ItemStack> cir) {
-		if (ModConfig.get().disableCurativeItems)
-			cir.setReturnValue(stack);
+	@WrapOperation(
+			method = "finishUsingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;removeEffect(Lnet/minecraft/world/effect/MobEffect;)Z"))
+	public boolean raspberry$disableCuring(LivingEntity instance, MobEffect mobeffectinstance, Operation<Boolean> original) {
+		if (!ModConfig.get().disableCurativeItems)
+			original.call(instance, mobeffectinstance);
+		return false;
 	}
 }
